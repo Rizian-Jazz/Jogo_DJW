e8d3bc87a1fed64af12b29bc4bdf1b2fa6e191a3
Rizian <rizianjazz@gmail.com>
2026-03-29 19:30:27 -0300

    ajustes no script e na unity

--- Assets/Scripts/Tiro.cs
+++ Assets/Scripts/Tiro.cs
@@ -9,39 +9,25 @@ public class Tiro : MonoBehaviour
     public GameObject bulletPrefab;
     public UnityEvent fireEvent;
     public float bulletSpeed = 10f;
-    public Rigidbody2D rb;
      Vector2 bulletVelocity;
      Vector2 bulletDirection;
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.gameObject.CompareTag("Collider"))
-        {
-            Destroy(gameObject);
-        }
-    }
+     public Transform firePoint; //arma
 
     public void Fire(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            GameObject bullet = Instantiate(bulletPrefab,firePoint.position, firePoint.rotation);
             Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
 
-            bulletDirection = context.ReadValue<Vector2>();
-            bulletVelocity = transform.right * bulletSpeed;
+            bulletDirection = firePoint.right;
+            bulletVelocity =  bulletSpeed * bulletDirection;
             bulletRb.linearVelocity = bulletVelocity;
             fireEvent.Invoke();
         }
+
     }
+
+
+    
 }
