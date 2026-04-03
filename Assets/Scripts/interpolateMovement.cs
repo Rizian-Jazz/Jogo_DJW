9dcdd495f77aa147c06cb6176f24f468ed33c10e
Rizian <rizianjazz@gmail.com>
2026-04-03 16:20:01 -0300

    tudo pro dev
    sla deu um erro e tudo eu coloquei no dev

--- Assets/Scripts/interpolateMovement.cs
+++ Assets/Scripts/interpolateMovement.cs
@@ -1,35 +1,11 @@
-<<<<<<< Updated upstream
-using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.Scripting.APIUpdating;
-=======
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
->>>>>>> Stashed changes
 
 public class interpolateMovement : MonoBehaviour
 {
 
 
-<<<<<<< Updated upstream
-    public float speed = 6f;
-    public float acceleration = 10f;
-    public float deceleration = 5f;
-    public Rigidbody2D rb;
-    private float horizontalMovement;
-    private float verticalMovement;
-    private Vector2 targetVelocity;
-    private Vector2 moveInput;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-=======
     public float speed = 6f, acceleration = 10f, deceleration = 5f;
     public Rigidbody2D rb;
     private float horizontalMovement, verticalMovement;
@@ -38,7 +14,6 @@ public class interpolateMovement : MonoBehaviour
 
     public bool canThrow = true;
 
->>>>>>> Stashed changes
     void Update()
     {
         targetVelocity = (moveInput.normalized * speed);
@@ -50,11 +25,7 @@ public class interpolateMovement : MonoBehaviour
         
         float rate;
         {
-<<<<<<< Updated upstream
-            if (targetVelocity.magnitude > 0)
-=======
             if (targetVelocity.magnitude > 0) //magnitude é a distância do vetor até a origem, ou seja, o comprimento do vetor (riza)
->>>>>>> Stashed changes
             {
                 rate = acceleration;
             }
@@ -70,9 +41,6 @@ public class interpolateMovement : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
-<<<<<<< Updated upstream
-    }
-=======
         if (moveInput.magnitude > 0) 
         {
             lastInput = moveInput;  
@@ -88,5 +56,4 @@ public class interpolateMovement : MonoBehaviour
         }
 
   
->>>>>>> Stashed changes
 }
