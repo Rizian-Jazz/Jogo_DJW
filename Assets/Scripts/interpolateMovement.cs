1cd2eb27433078d3b36feafa490a2a622343ee78
Rizian <rizianjazz@gmail.com>
2026-04-03 16:18:37 -0300

    tudo pro dev
    sla deu um erro e tudo eu coloquei no dev

--- Assets/Scripts/interpolateMovement.cs
+++ Assets/Scripts/interpolateMovement.cs
@@ -1,11 +1,18 @@
+<<<<<<< Updated upstream
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Scripting.APIUpdating;
+=======
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+>>>>>>> Stashed changes
 
 public class interpolateMovement : MonoBehaviour
 {
 
 
+<<<<<<< Updated upstream
     public float speed = 6f;
     public float acceleration = 10f;
     public float deceleration = 5f;
@@ -22,6 +29,16 @@ public class interpolateMovement : MonoBehaviour
     }
 
     // Update is called once per frame
+=======
+    public float speed = 6f, acceleration = 10f, deceleration = 5f;
+    public Rigidbody2D rb;
+    private float horizontalMovement, verticalMovement;
+    private Vector2 targetVelocity, moveInput;
+    public Vector2 lastInput;
+
+    public bool canThrow = true;
+
+>>>>>>> Stashed changes
     void Update()
     {
         targetVelocity = (moveInput.normalized * speed);
@@ -33,7 +50,11 @@ public class interpolateMovement : MonoBehaviour
         
         float rate;
         {
+<<<<<<< Updated upstream
             if (targetVelocity.magnitude > 0)
+=======
+            if (targetVelocity.magnitude > 0) //magnitude é a distância do vetor até a origem, ou seja, o comprimento do vetor (riza)
+>>>>>>> Stashed changes
             {
                 rate = acceleration;
             }
@@ -49,5 +70,23 @@ public class interpolateMovement : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+<<<<<<< Updated upstream
     }
+=======
+        if (moveInput.magnitude > 0) 
+        {
+            lastInput = moveInput;  
+        }
+
+    }
+
+    public IEnumerator Throw()
+    {
+        canThrow = false;
+        yield return new WaitForSeconds(0.5f);
+        canThrow = true;
+        }
+
+  
+>>>>>>> Stashed changes
 }
